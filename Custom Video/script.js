5a87f38328160c7248545b867e651ef7a3d635dc
Kyllian Creppy <kyllian.creppy@outlook.fr>
2021-10-06 13:55:13 +0200

    Css added

--- Custom Video/script.js
+++ Custom Video/script.js
@@ -4,7 +4,7 @@ const video = player.querySelector('.viewer');
 const progress = player.querySelector('.progress');
 const progressBar = player.querySelector('.progress__filled');
 const toggle = player.querySelector('.toggle');
-const skipButtons = player.querySelector('[data-shkip]');
+const skipButtons = player.querySelector('[data-skip]');
 const ranges = player.querySelector('.player__slider');
 
 /*Build out functions */
