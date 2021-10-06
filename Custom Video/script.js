8373a48aa657657b69a54db89bdb84d74582bd7e
Kyllian Creppy <kyllian.creppy@outlook.fr>
2021-10-06 13:45:04 +0200

    Video script

---
+++ Custom Video/script.js
@@ -0,0 +1,17 @@
+/*Get out Elements */
+const player = document.querySelector('.player');
+const video = player.querySelector('.viewer');
+const progress = player.querySelector('.progress');
+const progressBar = player.querySelector('.progress__filled');
+const toggle = player.querySelector('.toggle');
+const skipButtons = player.querySelector('[data-shkip]');
+const ranges = player.querySelector('.player__slider');
+
+/*Build out functions */
+function togglePlay() {
+    if(video.paused)  {
+        video.played();
+    } else {
+            video.pause;
+    }
+}
